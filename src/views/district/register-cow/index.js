2bd2f31d0ce7f335a14861aca72028e325864253
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-12-18 01:59:07 +0200

    register new cow

--- src/views/district/register-cow/index.js
+++ src/views/district/register-cow/index.js
@@ -12,22 +12,42 @@ import { BACKEND_URL } from "src/constants";
 import { errorHandler, toastMessage } from "src/helpers";
 import { useDispatch, useSelector } from "react-redux";
 import { setShowFullPageLoader } from "src/actions/app";
-import { setUserFullName, setUserEmail, setUserPhone } from "src/actions/user";
 
+const initialState = {
+  cowNumber: "",
+  cowType: "",
+  registrationStatus: "",
+  registrationKg: "",
+  supplierName: "",
+};
 function RegisterCow() {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.user);
-  const [state, setState] = useState({
-    fullName: "",
-    email: "",
-    phone: "",
-  });
+
+  const [state, setState] = useState(initialState);
 
   const changeHandler = (e) => {
     setState({ ...state, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(setShowFullPageLoader(true));
+    Axios.post(BACKEND_URL + "/cows/", { ...state, token })
+      .then((res) => {
+        setTimeout(() => {
+          dispatch(setShowFullPageLoader(false));
+          toastMessage("success", res.data.msg);
+          setState(initialState);
+        }, 1000);
+      })
+      .catch((error) => {
+        setTimeout(() => {
+          errorHandler(error);
+          dispatch(setShowFullPageLoader(false));
+        }, 1000);
+      });
+  };
 
   return (
     <CRow>
@@ -40,13 +60,70 @@ function RegisterCow() {
             <CCardBody>
               <div className="row">
                 <div className="col-md-4 mb-3">
-                  <label>Names</label>
+                  <label>Cow Number</label>
                   <input
-                    type="text"
-                    name="fullName"
+                    type="number"
+                    name="cowNumber"
                     className="form-control"
                     required
-                    placeholder="Your names"
+                    placeholder="Unique cow number"
+                    value={state.cowNumber}
+                    onChange={(e) => changeHandler(e)}
+                  />
+                </div>
+                <div className="col-md-4 mb-3">
+                  <label>Cow Type</label>
+                  <select
+                    name="cowType"
+                    className="form-control"
+                    required
+                    value={state.cowType}
+                    onChange={(e) => changeHandler(e)}
+                  >
+                    <option value="">Choose</option>
+                    <option value="Frizone">Frizone</option>
+                    <option value="Jelsie">Jelsie</option>
+                    <option value="Mixed">Mixed</option>
+                  </select>
+                </div>
+                <div className="col-md-4 mb-3">
+                  <label>Status</label>
+                  <select
+                    name="registrationStatus"
+                    className="form-control"
+                    required
+                    value={state.registrationStatus}
+                    onChange={(e) => changeHandler(e)}
+                  >
+                    <option value="">Choose</option>
+                    <option value="Imbyeyi">Imbyeyi</option>
+                    <option value="Ishashi">Ishashi</option>
+                    <option value="Ihaka">Ihaka</option>
+                  </select>
+                </div>
+                <div className="col-md-4 mb-3">
+                  <label>Kilograms</label>
+                  <input
+                    type="number"
+                    name="registrationKg"
+                    className="form-control"
+                    required
+                    placeholder="Unique kilograms"
+                    value={state.registrationKg}
+                    onChange={(e) => changeHandler(e)}
+                  />
+                </div>
+
+                <div className="col-md-4 mb-3">
+                  <label>Supplier Names</label>
+                  <input
+                    type="text"
+                    name="supplierName"
+                    className="form-control"
+                    required
+                    placeholder="Enter supplier names"
+                    value={state.supplierName}
+                    onChange={(e) => changeHandler(e)}
                   />
                 </div>
               </div>
